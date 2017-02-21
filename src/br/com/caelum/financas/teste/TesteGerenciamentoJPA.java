26fadb5b00c7579d7d705101a5c3312948f601d9
Davi Leitão <davi.leitao@hotmail.com>
2017-02-21 15:22:41 -0300

    Creating TesteGerenciamentoJPA class

---
+++ src/br/com/caelum/financas/teste/TesteGerenciamentoJPA.java
@@ -0,0 +1,26 @@
+package br.com.caelum.financas.teste;
+
+import javax.persistence.EntityManager;
+
+import br.com.caelum.financas.modelo.Conta;
+import br.com.caelum.financas.util.JPAUtil;
+
+public class TesteGerenciamentoJPA {
+
+	public static void main(String[] args) {
+		
+		EntityManager manager = new JPAUtil().getEntityManager();
+	
+		manager.getTransaction().begin();
+		Conta conta = manager.find(Conta.class, 1);
+		manager.getTransaction().commit();
+		
+		conta.setTitular("New owner");
+
+		manager.getTransaction().begin();
+		manager.merge(conta);
+		manager.getTransaction().commit();
+		
+		manager.close();
+	}
+}
