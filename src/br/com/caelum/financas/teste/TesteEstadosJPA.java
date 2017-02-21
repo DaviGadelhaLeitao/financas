2a8f259978b09009c474067ab8e5b69694ec0e2a
Davi Leitão <davi.leitao@hotmail.com>
2017-02-21 14:49:03 -0300

    Searching database with find() and getting a managed transactional state

--- src/br/com/caelum/financas/teste/TesteEstadosJPA.java
+++ src/br/com/caelum/financas/teste/TesteEstadosJPA.java
@@ -2,6 +2,7 @@ package br.com.caelum.financas.teste;
 
 import javax.persistence.EntityManager;
 
+import br.com.caelum.financas.modelo.Conta;
 import br.com.caelum.financas.util.JPAUtil;
 
 public class TesteEstadosJPA {
@@ -9,12 +10,14 @@ public class TesteEstadosJPA {
 	public static void main(String[] args) {
 
 		EntityManager manager = new JPAUtil().getEntityManager();
+		
+		Conta conta = manager.find(Conta.class, 1);
 
 		manager.getTransaction().begin();
 
-		// Testes do capitulo
-
 		manager.getTransaction().commit();
+		
+		System.out.println(conta.getTitular());
 
 		manager.close();
 
